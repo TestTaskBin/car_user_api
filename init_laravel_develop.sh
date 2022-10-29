d9e2da930ef8da7f271d6ed7589df0817ea0239a
Eugene Popov <popoven@ngs.ru>
2022-10-30 03:37:07 +0700

    Исправления по итогам финального тестирования

--- init_laravel_develop.sh
+++ init_laravel_develop.sh
@@ -13,10 +13,21 @@ docker run -it --user "$XUID:$XGID" \
      composer:2.4 composer create-project --no-cache laravel/laravel . 9.3 || exit 1
 fi
 
+# compose config update
+if test ! -f "$DIR/docker-compose.yml"
+then
+cp "$DIR/templates/docker-compose.yml"  "$DIR/docker-compose.yml" || exit 1
+#cp "$DIR/templates/docker-compose_host.yml"  "$DIR/docker-compose.yml" || exit 1
+sed -i "s/%USER%/${XUID}:${XGID}/"  "$DIR/docker-compose.yml" || exit 1
+fi
+
 # first run after chckout
 if test ! -f "$DIR/app/.env"
 then
 cp "$DIR/app/.env.example"  "$DIR/app/.env" || exit 1
+docker run -it --user "$XUID:$XGID" \
+    -v "$DIR/app:/app" --workdir "/app" \
+     composer:2.4 composer install || exit 1
 docker run -it --user "$XUID:$XGID" \
     -v "$DIR/app:/app" --workdir "/app" \
      composer:2.4 php artisan migrate --force || exit 1
@@ -28,11 +39,4 @@ docker run -it --user "$XUID:$XGID" \
      composer:2.4 php artisan db:seed --force || exit 1
 fi
 
-# compose config update
-if test ! -f "$DIR/docker-compose.yml"
-then
-cp "$DIR/templates/docker-compose.yml"  "$DIR/docker-compose.yml" || exit 1
-#cp "$DIR/templates/docker-compose_host.yml"  "$DIR/docker-compose.yml" || exit 1
-sed -i "s/%USER%/${XUID}:${XGID}/"  "$DIR/docker-compose.yml" || exit 1
-fi
 
